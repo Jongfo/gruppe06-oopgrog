6057d2c9dbd699fdc85ff2b13fa5f8f3ac3608fe
benjabe <benjabe@stud.ntnu.no>
2018-03-19 14:32:35 +0100

    Laga RobustIO::okAdr() og RobustIO::okNavn()

--- RobustIO.cpp
+++ RobustIO.cpp
@@ -64,15 +64,36 @@ char* RobustIO::lesTabelltypeFraFil(char* tabelltype)
 	delete[] tabelltype;
 }
 
+// returnerer true dersom berre bokstavar, mellomrom, bindestrek
 bool RobustIO::okNavn(char* s)
 {
-	// TODO
+	int len = strlen(s);
+	for (int i = 0; i < len; i++)
+	{
+		if (!((s[i] >= 'A' && s[i] <= 'Z') ||
+			(s[i] >= 'a' && s[i] <= 'z') ||
+			s[i] == '-' || s[i] == ' '))
+		{
+			return false;
+		}
+	}
 	return true;
 }
 
+// returnerer true dersom inneheld berre bokstavar, mellomrom, bindestrek og tal
 bool RobustIO::okAdr(char* s)
 {
-	// TODO
+	int len = strlen(s);
+	for (int i = 0; i < len; i++)
+	{
+		if (!((s[i] >= 'A' && s[i] <= 'Z') ||
+			  (s[i] >= 'a' && s[i] <= 'z') ||
+			  (s[i] >= '0' && s[i] <= '9') ||
+			   s[i] == '-' || s[i] == ' '))
+		{
+			return false;
+		}
+	}
 	return true;
 }
 
